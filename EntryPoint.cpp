2c0aca1fde5c96038c57aa3ff73689402d4b64a3
Corentin <corentinboblet@gmail.com>
2025-03-22 16:15:25 +0100

    Added start of a camera

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -8,10 +8,21 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "StarField2D", sf::Style::Close, sf::State::Windowed);
-	std::cout << "Hello World" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "StarField2D", sf::Style::Close, sf::State::Windowed);
+    std::cout << "Hello World" << std::endl;
 
+    // Background
+    sf::Texture windowTexture("res/PSY.png");
+    windowTexture.setRepeated(true);
+    sf::RectangleShape rectangleWindow({800*10,800*10});
+    rectangleWindow.setPosition({0,0});
+    rectangleWindow.setOrigin({rectangleWindow.getSize().x / 2, rectangleWindow.getSize().y / 2});
+    rectangleWindow.setTexture(&windowTexture);
+    rectangleWindow.setTextureRect({{0,0}, {static_cast<int>(windowTexture.getSize().x*10),static_cast<int>(windowTexture.getSize().y*10)}});
+    
     Player player;
+    sf::View cameraView({0.f,0.f}, {400.f, 400.f});
+    sf::Clock deltaTime; deltaTime.start();
 
     while (window.isOpen())
     {
@@ -27,11 +38,16 @@ int main()
             if (const sf::Event::KeyReleased* keyReleased = event->getIf<sf::Event::KeyReleased>())
                 player.HandleInput(keyReleased->scancode, false);
         }
-        player.Move();
+        player.Move(deltaTime.getElapsedTime().asSeconds());
+
+        cameraView.setCenter(player.GetPosition());
+        window.setView(cameraView);
 
         window.clear();
+        window.draw(rectangleWindow);
         window.draw(player.GetSprite());
         window.display();
+        deltaTime.restart();
     }
 
 	return 0;
