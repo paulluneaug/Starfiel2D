8f39ed85a06a745e2396f690c8385907a2fe0c94
p.luneau <paul.luneau@mail.com>
2025-04-03 21:27:08 +0200

    [MOD] Added the background to the entry point

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -3,57 +3,65 @@
 
 #include <iostream>
 
+#include "Background.h"
 #include "Camera.hpp"
-#include "Player.hpp"
 #include "Lib.h"
+#include "Player.hpp"
 
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "StarField2D", sf::Style::Close, sf::State::Windowed);
-    std::cout << "Hello World" << std::endl;
+	sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "StarField2D", sf::Style::Close, sf::State::Windowed);
+	std::cout << "Hello World" << std::endl;
 
-    // Background
-    
-    Player player;
-    Camera camera;
+	// Background
+
+	GenerationSetings generationSettings;
+	generationSettings.PlanetsSizeRange = { 0.1f, 0.5f };
+	generationSettings.PlanetProbability = 0.8f;
+
+	Background background{ generationSettings };
+
+	Player player;
+	Camera camera;
 
 
-    double frameStart = 0.0;
-    double frameEnd = 0.0;
+	double frameStart = 0.0;
+	double frameEnd = 0.0;
 
-    while (window.isOpen())
-    {
-        float deltaTime = static_cast<float>(frameEnd - frameStart);
-        frameStart = Lib::GetTimeStamp();
+	while (window.isOpen())
+	{
+		float deltaTime = static_cast<float>(frameEnd - frameStart);
+		frameStart = Lib::GetTimeStamp();
 
-        // check all the window's events that were triggered since the last iteration of the loop
-        while (const std::optional event = window.pollEvent())
-        {
-            // "close requested" event: we close the window
-            if (event->is<sf::Event::Closed>())
-                window.close();
+		// check all the window's events that were triggered since the last iteration of the loop
+		while (const std::optional event = window.pollEvent())
+		{
+			// "close requested" event: we close the window
+			if (event->is<sf::Event::Closed>())
+				window.close();
 
-            if (const sf::Event::KeyPressed* keyPressed = event->getIf<sf::Event::KeyPressed>())
-                player.HandleInput(keyPressed->scancode, true);
-            if (const sf::Event::KeyReleased* keyReleased = event->getIf<sf::Event::KeyReleased>())
-                player.HandleInput(keyReleased->scancode, false);
-        }
-        player.Move(deltaTime);
+			if (const sf::Event::KeyPressed* keyPressed = event->getIf<sf::Event::KeyPressed>())
+				player.HandleInput(keyPressed->scancode, true);
+			if (const sf::Event::KeyReleased* keyReleased = event->getIf<sf::Event::KeyReleased>())
+				player.HandleInput(keyReleased->scancode, false);
+		}
+		player.Move(deltaTime);
 
-        // Camera work
-        camera.ChangePosition(player.GetPosition());
-        camera.UpdateView(window);
+		// Camera work
+		camera.ChangePosition(player.GetPosition());
+		camera.UpdateView(window);
 
-        window.clear();
-        
-        camera.Draw(window);
-        player.Draw(window);
-        
-        window.display();
+		window.clear();
 
-        frameEnd = Lib::GetTimeStamp();
-    }
+		background.Draw(window, camera);
+		camera.Draw(window);
+		player.Draw(window);
+
+		window.display();
+
+		frameEnd = Lib::GetTimeStamp();
+	}
 
 	return 0;
 }
